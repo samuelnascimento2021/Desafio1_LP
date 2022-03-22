3f39e1afcc9b2e08a76367e9ba652851142e396e
Samuel Brayan <85649169+samuelnascimento2021@users.noreply.github.com>
2022-03-21 22:09:45 -0300

    Update Exercicio3_Desafio1.java

--- Exercicio3_Desafio1.java
+++ Exercicio3_Desafio1.java
@@ -1,8 +1,31 @@
 package Exercicios_Desafio1;
 public class Exercicio3_Desafio1 {
 public static void main(String[] args) {
-	 
-
 		
+	Scanner ler = new Scanner(System.in);
+		
+	int num;
+		
+	System.out.println("Insira um número");
+	num = ler.nextInt();
+		
+	Integer.toString(num);
+	
+	String rain = "";
+		
+	if (num % 3 == 0) {
+            rain += "Pling";
+        }
+        if (num % 5 == 0) {
+            rain += "Plang";
+        }
+        if (num % 7 == 0) {
+            rain += "Plong";
+        }
+        if (rain== "");
+		
+        System.out.println(rain);
+		
+		}
 	}
-}
+
